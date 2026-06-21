546e761780b7d76443c1bab94fd52593453e74fc
bcalou <bastien.calou@gmail.com>
2026-06-21 14:34:24 +0200

    wip

---
+++ src/@types/disconnect.d.ts
@@ -0,0 +1,114 @@
+// Type declarations for the 'disconnect' Discogs API client
+// Project: https://github.com/discogs/disconnect
+// Definitions by: [Your Name]
+
+declare module "disconnect" {
+  import { EventEmitter } from "events";
+
+  // --- Core Types ---
+  export interface DiscogsClientOptions {
+    consumerKey: string;
+    consumerSecret: string;
+    userToken?: string;
+    userSecret?: string;
+    oauth?: {
+      consumerKey: string;
+      consumerSecret: string;
+      token?: string;
+      tokenSecret?: string;
+    };
+  }
+
+  export interface PaginationOptions {
+    page?: number;
+    per_page?: number;
+  }
+
+  export interface RequestOptions {
+    method?: "GET" | "POST" | "PUT" | "DELETE";
+    body?: any;
+    headers?: Record<string, string>;
+    query?: Record<string, any>;
+  }
+
+  // --- Database API ---
+  export interface DatabaseAPI {
+    getArtist(id: number): Promise<any>;
+    getArtistReleases(id: number, options?: PaginationOptions): Promise<any>;
+    getRelease(id: number): Promise<any>;
+    getMaster(id: number): Promise<any>;
+    getMasterVersions(id: number): Promise<any>;
+    search(
+      query: string,
+      options?: { type?: string; page?: number; per_page?: number }
+    ): Promise<any>;
+    // Add other database methods as needed
+  }
+
+  // --- User API ---
+  export interface UserAPI {
+    getIdentity(): Promise<any>;
+    getCollectionFolders(user: string): Promise<any>;
+    getCollectionReleases(
+      user: string,
+      folderId?: number,
+      options?: PaginationOptions
+    ): Promise<any>;
+    // Add other user methods as needed
+  }
+
+  // --- Marketplace API ---
+  export interface MarketplaceAPI {
+    getListing(id: number): Promise<any>;
+    getInventory(user: string, options?: PaginationOptions): Promise<any>;
+    // Add other marketplace methods as needed
+  }
+
+  // --- Main Client ---
+  export interface DiscogsClient {
+    database(): DatabaseAPI;
+    user(): UserAPI;
+    marketplace(): MarketplaceAPI;
+    setToken(token: string, secret: string): void;
+    oauth(): {
+      getRequestToken: (
+        callbackUrl: string,
+        permissions: string[],
+        callback: (err: Error | null, data: any) => void
+      ) => void;
+      getAccessToken: (
+        verifier: string,
+        requestToken: string,
+        requestTokenSecret: string,
+        callback: (err: Error | null, data: any) => void
+      ) => void;
+    };
+    request(path: string, options?: RequestOptions): Promise<any>;
+  }
+
+  // --- Constructor ---
+  export class Client extends EventEmitter {
+    constructor(options: DiscogsClientOptions);
+    database(): DatabaseAPI;
+    user(): UserAPI;
+    marketplace(): MarketplaceAPI;
+    setToken(token: string, secret: string): void;
+    oauth: () => {
+      getRequestToken: (
+        callbackUrl: string,
+        permissions: string[],
+        callback: (err: Error | null, data: any) => void
+      ) => void;
+      getAccessToken: (
+        verifier: string,
+        requestToken: string,
+        requestTokenSecret: string,
+        callback: (err: Error | null, data: any) => void
+      ) => void;
+    };
+    request(path: string, options?: RequestOptions): Promise<any>;
+  }
+
+  // --- Exports ---
+  // export = Client;
+}
--- src/classes/Api.ts
+++ src/classes/Api.ts
@@ -5,66 +5,138 @@ import {
   DCRelease,
   DCVersions,
 } from "../types";
-
-// @ts-ignore
 import * as Discogs from "disconnect";
+import Database from "better-sqlite3";
+import path from "path";
 
 export const DELAY = 1000;
 export const PER_PAGE = 100;
+export const API_CONSUMER_KEY = "KjrpvorlXakACBzWYgvl";
+export const API_CONSUMER_SECRET = "CggkIagphcuYfzlkoqElKiVtyyNsYZMR";
+export const API_CACHE_FILE = "../../discogs_cache.db";
+
+type CacheEntry = {
+  key: string;
+  value: string;
+};
 
 export class Api {
+  // The singleton instance
   static instance: Api;
-  private database: any;
+
+  // The discogs database connector
+  private discogs: Discogs.DatabaseAPI;
+
+  // The sqlite cache
+  private cache: Database.Database;
 
   constructor() {
+    // Satisfy Typescript instanciation in constructor requirements
+    this.cache = Api.instance?.cache;
+    this.discogs = Api.instance?.discogs;
+
     if (!!Api.instance) {
       return Api.instance;
     }
 
     Api.instance = this;
 
-    this.database = new Discogs.Client({
-      consumerKey: "KjrpvorlXakACBzWYgvl",
-      consumerSecret: "CggkIagphcuYfzlkoqElKiVtyyNsYZMR",
-    }).database();
+    this.init();
   }
 
   public async getArtist(id: number): Promise<DCArtist> {
-    await this.delay();
-    return this.database
-      .getArtist(id)
-      .then((result: any) => DCArtist.parse(result));
+    const key = `artist_${id}`;
+
+    return this.fetchAndCache<DCArtist>(key, () =>
+      this.discogs.getArtist(id).then((result: any) => DCArtist.parse(result))
+    );
   }
 
   public async getArtistReleases(
     id: number,
     page = 1
   ): Promise<DCArtistReleases> {
-    await this.delay();
-    return this.database
-      .getArtistReleases(id, { page: page, per_page: PER_PAGE })
-      .then((result: any) => DCArtistReleases.parse(result));
+    const key = `artist_releases_${id}_${page}`;
+    return this.fetchAndCache<DCArtistReleases>(key, () =>
+      this.discogs
+        .getArtistReleases(id, { page: page, per_page: PER_PAGE })
+        .then((result: any) => DCArtistReleases.parse(result))
+    );
   }
 
   public async getRelease(id: number): Promise<DCRelease> {
-    await this.delay();
-    return this.database
-      .getRelease(id)
-      .then((result: any) => DCRelease.parse(result));
+    const key = `release_${id}`;
+    return this.fetchAndCache<DCRelease>(key, () =>
+      this.discogs.getRelease(id).then((result: any) => DCRelease.parse(result))
+    );
   }
 
   public async getMaster(id: number): Promise<DCMaster> {
-    await this.delay();
-    return this.database
-      .getMaster(id)
-      .then((result: any) => DCMaster.parse(result));
+    const key = `master_${id}`;
+    return this.fetchAndCache<DCMaster>(key, () =>
+      this.discogs.getMaster(id).then((result: any) => DCMaster.parse(result))
+    );
   }
 
   public async getVersions(id: number): Promise<DCVersions> {
+    const key = `versions_${id}`;
+    return this.fetchAndCache<DCVersions>(key, () =>
+      this.discogs
+        .getMasterVersions(id)
+        .then((result: any) => DCVersions.parse(result))
+    );
+  }
+
+  // Main initializer
+  private init() {
+    this.discogs = new Discogs.Client({
+      consumerKey: API_CONSUMER_KEY,
+      consumerSecret: API_CONSUMER_SECRET,
+    }).database();
+
+    const dbPath = path.resolve(__dirname, API_CACHE_FILE);
+    this.cache = new Database(dbPath);
+    this.initCacheTable();
+  }
+
+  // Create a cache table
+  private initCacheTable(): void {
+    this.cache?.exec(`
+      CREATE TABLE IF NOT EXISTS cache (
+        key TEXT PRIMARY KEY,
+        value TEXT
+      )
+    `);
+  }
+
+  // Récupère ou stocke dans le cache
+  private async getCached<ResultType>(
+    key: string,
+    fetcher: () => Promise<ResultType>
+  ): Promise<ResultType | null> {
+    if (!this.cache) return null;
+
+    const row = this.cache
+      .prepare("SELECT * FROM cache WHERE key = ?")
+      .get(key) as CacheEntry;
+
+    return row
+      ? (JSON.parse(row.value) as ResultType)
+      : this.fetchAndCache(key, fetcher);
+  }
+
+  // Fetch the data and add it to the cache
+  private async fetchAndCache<ResultType>(
+    key: string,
+    fetcher: () => Promise<ResultType>
+  ): Promise<ResultType> {
+    const data = await fetcher();
     await this.delay();
-    return this.database
-      .getMasterVersions(id)
-      .then((result: any) => DCVersions.parse(result));
+
+    this.cache
+      .prepare("INSERT OR REPLACE INTO cache (key, value) VALUES (?, ?)")
+      .run(key, JSON.stringify(data));
+    return data;
   }
 
   private async delay() {
